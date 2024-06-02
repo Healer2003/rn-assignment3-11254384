93d3cf127fdfc11d3c9b070506f480d8aa16af93
Healer2003 <wwww.wilberforce221@gmail.com>
2024-06-02 11:05:42 +0000

    Folder containing custom  components

---
+++ Components/ScrollItems.js
@@ -0,0 +1,97 @@
+export const category = [
+    {
+
+    name:"Exercise",
+    task:"12 Tasks",
+    image:require('../assets/pic1.png'),
+    id:'1'
+    
+    },
+    {
+        name:"Study",
+        task:"12 Tasks",
+        image:require('../assets/pic2.png'),
+        id:'2'
+    },
+    {
+        name:"Code",
+        task:"12 Tasks",
+        image:require('../assets/coding.png'),
+        id:'3'
+    },
+    {
+        name:"Ride",
+        task:"12 Tasks",
+        image:require('../assets/ride.png'),
+        id:'4'
+    },
+    {
+        name:"Relax",
+        task:"12 Tasks",
+        image:require('../assets/relax.png'),
+        id:'5'
+    },
+]
+
+export const tasks = [
+    {
+        name:"Mobile App Development",
+        id:"1"
+    },
+    {
+        name:"Web Dev",
+        id:"2"
+    },
+    {
+        name:"Push Ups",
+        id:"3"
+    },
+    {
+        name:"Exercise",
+        id:"4"
+    },
+    {
+        name:"Relax",
+        id:"5"
+    },
+    {
+        name:"Run",
+        id:"6"
+    },
+    {
+        name:"Machine Learning",
+        id:"7"
+    },
+    {
+        name:"Cybersecurity",
+        id:"8"
+    },
+    {
+        name:"Read",
+        id:"9"
+    },
+    {
+        name:"Walk",
+        id:"10"
+    },
+    {
+        name:"Swim",
+        id:"11"
+    },
+    {
+        name:"Drive",
+        id:"12"
+    },
+    {
+        name:"Surf",
+        id:"13"
+    },
+    {
+        name:"Hike",
+        id:"14"
+    },
+    {
+        name:"Videogames",
+        id:"15"
+    }
+]
